5d7f9bd9dd16532641af5856f70f0ab93e242011
sanskar <sanskardagade07@gmail.com>
2025-05-03 15:45:33 +0530

    Bit++

---
+++ A. Bit++.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+bool checkStatement(string statement){
+    if(statement == "X++" || statement == "++X") return true;
+    else return false;
+}
+
+int main(){
+    int n; // number of sttements
+    cin>>n;
+
+    int x = 0;
+
+    for(int i=0;i<n;i++){
+        string statement;
+        cin >> statement;
+
+        if(checkStatement(statement)) x++;
+        else x--;
+    }
+
+    cout<<x<<"\n";
+    return 0;
+
+
+}
