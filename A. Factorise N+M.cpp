7ae5fc182afea369736ad2b92155e396a508301c
sanskardagade <sanskardagade07@gmail.com>
2025-06-10 23:25:04 +0530

    left with some topic

---
+++ A. Factorise N+M.cpp
@@ -0,0 +1,20 @@
+/*
+https://codeforces.com/problemset/problem/1740/A
+*/
+
+#include<iostream>
+using namespace std;
+
+void solve(){
+    int n;
+    cin>>n;
+    cout<<"7\n";
+}
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){ 
+        solve();
+    }
+}
